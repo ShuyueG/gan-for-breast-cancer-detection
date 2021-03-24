880ac4d71c9f6474f6566312ac496ea5e1e84f0e
Shuyue (Frank) Guan <frankshuyueguan@gwu.edu>
2021-03-24 16:40:36 -0400

    Affine Transformations

--- Image_Aug.py
+++ Image_Aug.py
@@ -1,3 +1,4 @@
+### Image Augmentation by Affine Transformation
 
 from keras.preprocessing.image import ImageDataGenerator
 import time, json, os
@@ -5,13 +6,12 @@ import time, json, os
 
 # dimensions of our images.
 img_width, img_height = 320, 320
-
-AbsLoc = 'D:\GAN\dcgan'
-# top_model_weights_path = 'fc_model.h5'
-data_dir = os.path.join(AbsLoc, 'data/train/norm')
-
 batch_size = 26
 
+AbsLoc = 'D:\dataset_root_directory'
+data_dir = os.path.join(AbsLoc, 'data_inpit_folder')  # input data
+
+# To apply Affine Transformation
 datagen = ImageDataGenerator(rotation_range=30,
                              width_shift_range=0.2,
                              height_shift_range=0.2,
@@ -27,7 +27,7 @@ generator = datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode=None,
     shuffle=True,
-    save_to_dir=os.path.join(AbsLoc, 'data/preview'), save_prefix='more_norm', save_format='png')
+    save_to_dir=os.path.join(AbsLoc, 'data_output_folder'), save_prefix='data_name', save_format='png')
 
 
 i = 1
@@ -35,4 +35,4 @@ for batch in generator:
     i += 1
     if i > 45:
         break
-
+        
